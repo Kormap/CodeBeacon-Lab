73bb44170cdfc83cd66cc33b344751600cb9d07b
고동혁/TANine/q904nu <q904nu@ssg.global>
2022-11-10 16:58:48 +0900

    APP메뉴 테스트 API commit

--- src/main/java/com/codebeacon/APIController.java
+++ src/main/java/com/codebeacon/APIController.java
@@ -1,52 +1,37 @@
 package com.codebeacon;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
-public class PageController {
-	
-	String welcometext = "";
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//API 확인용 = RestController 로 생성
+@RestController
+@RequestMapping("/api/http/*")
+public class APIController {
 	
 	@GetMapping("/useragent")
-	public String useragent(Model model, HttpServletRequest request, HttpServletResponse response) {
+	public String getuserAgent(HttpServletRequest request, HttpServletResponse response) {
 		
-		String userAgent = request.getHeader("User-Agent"); System.out.println("USER-AGENT = "+ userAgent);
-		
-		//상태코드 강제 설정 
-		response.setStatus(500);
-		
-		//응답헤더 - 윈도우설정 
-//		userAgent = "Windows";
-//		response.setHeader(userAgent, userAgent);
-		String os_name = "";
-		if(userAgent.indexOf("Mac") > -1 ) { 
-			System.out.println("User-Agent : 맥북 이용자시네요");
-			os_name = "Mac";
-			welcometext = "맥북을 쓰시다니 고수시군요!";
-		}else if(userAgent.indexOf("Windows") > -1 ) { 
-			System.out.println("User-Agent : 윈도우 이용자시네요");
-			os_name = "Windows";
-			welcometext = "맥북 쓰세요.";
+		String userAgent = request.getHeader("User-Agent");
+		System.out.println("USER-AGENT = "+ userAgent);
+		response.setStatus(300);
+//		String os_name = "Mac";
+		if(userAgent.indexOf("Mac") > -1 ) System.out.println("User-Agent : 맥북 이용자시네요");
+		else if(userAgent.indexOf("Windows") > -1 ) System.out.println("User-Agent : 윈도우 이용자시네요");
 
-		 }
-		
-		model.addAttribute("data",userAgent);
-		model.addAttribute("os_name", os_name);
-		model.addAttribute("welcometext", welcometext);
-		return "useragent";
+		return userAgent;
 	}
 
-	/*
+
 	@GetMapping("/appmenulist")
-	public String getappMenuList(Model model) {
+	public List<AppMenuDept> getappMenuList(Model model) {
 
 		// List 만들기
 		List<AppMenuDept> appMenuList = new ArrayList<AppMenuDept>();
@@ -103,16 +88,16 @@ public class PageController {
 
 		appMenuList.add(appMenuDept1);
 
-		System.out.println("1="+appMenuList.get(0).getCd());
-		System.out.println("2="+appMenuList.get(1).getCd());
+		System.out.println("1= "+appMenuList.get(0).getCd());
+		System.out.println("2= "+appMenuList.get(1).getCd());
+		System.out.println("2-1= "+appMenuList.get(1).toString());
 
+//		model.addAttribute("appMenuList",appMenuList);
 
-		model.addAttribute("appMenuList",appMenuList);
-
-
-		return "appmenulist";
+		return appMenuList;
 	}
-*/  // APPMENU_LIST 테스트
-
-
 }
+
+
+
+
